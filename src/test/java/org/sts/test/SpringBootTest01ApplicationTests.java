6e686af2f4367a61ad07ebde2857e3d43adfd24c
tksidxj <tksidxj@gmail.com>
2021-09-06 17:04:00 +0900

    기본 프로젝트 정보 커밋

---
+++ src/test/java/org/sts/test/SpringBootTest01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sts.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTest01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
