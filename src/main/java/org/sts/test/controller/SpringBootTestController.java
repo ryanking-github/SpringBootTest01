341b272f3e6491ad209c118211a81edd805a9abf
gfdsls77 <gfdsls77@gmail.com>
2021-09-09 23:56:32 +0900

    Contoller 및 Template 테스트

--- src/main/java/org/sts/test/controller/SpringBootTestController.java
+++ src/main/java/org/sts/test/controller/SpringBootTestController.java
@@ -3,15 +3,21 @@ package org.sts.test.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.sts.test.domain.BookVO;
 
 @Controller
 public class SpringBootTestController {
 
-    @GetMapping("/hello")
-    public String getHello(){
+    @GetMapping("hello")
+    public String getHello(@RequestParam("title") String title){
         String result = "";
 
-        return result;
+        System.out.println("title = " + title);
+//        System.out.println("title : " + vo.getTitle());
+//        System.out.println("author = " + vo.getAuthor());
+
+        return "";
     }
 }
 
---
+++ src/main/java/org/sts/test/domain/BookVO.java
@@ -0,0 +1,12 @@
+package org.sts.test.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookVO {
+    private String title;
+    private String author;
+        
+}
